d4000c10441d8c943d824f31c45bb689fecd4389
madjid <mohamedmadjid.meddah@univ-mosta.dz>
2022-09-24 14:54:41 +0100

    [main]: add link between views , add login required functions to urls to restrict non login users to create posts

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from posts.views import BlogHome, BlogPostCreate, BlogPostUpdate, BlogPostDetail, BlogPostDelete
 
+from django.contrib.auth.decorators import login_required
+
 app_name = "posts"
 
 urlpatterns = [
     path('', BlogHome.as_view(), name='home'),
-    path('create/', BlogPostCreate.as_view(), name='create'),
+    path('create/', login_required(BlogPostCreate.as_view()), name='create'),
     path('<str:slug>/', BlogPostDetail.as_view(), name='post'),
     path('edit/<str:slug>/', BlogPostUpdate.as_view(), name='edit'),
     path('delete/<str:slug>/', BlogPostDelete.as_view(), name='delete'),
