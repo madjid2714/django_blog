e2f3af8fe26d945af5bf7824bcf20dd51c8a18eb
madjid <mohamedmadjid.meddah@univ-mosta.dz>
2022-10-08 20:39:13 +0100

    [main]: add pillow to requirement

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('posts.urls')),
+    path('', include('posts.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
